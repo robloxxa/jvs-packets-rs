793009277cc668e7581a018fd6221476ce68ffa5
robloxxa <robloxxa@yandex.ru>
2024-02-21 18:01:57 +0300

    Added tests

--- src/jvs.rs
+++ src/jvs.rs
@@ -51,36 +51,25 @@ impl_required_packet_blocks!(ResponsePacket);
 mod tests {
     use super::*;
 
-    // #[test]
-    // #[should_panic]
-    // fn test_request_packet_new_panic() {
-    //     let _ = RequestPacket::<1>::new();
-    // }
+    const REQUEST_DATA: [u8; 6] = [0xE0, 0xFF, 0x03, 0x01, 0x02, 0x05];
+    const RESPONSE_DATA: [u8; 7] = [0xE0, 0xFF, 0x04, 0x01, 0x01, 0x02, 0x07];
 
+    // Request Packet tests
     #[test]
     fn test_request_packet_from_slice() {
-        let data = [0xE0, 0, 3, 1, 2, 6];
-        let packet = RequestPacket::<256>::from_slice(&data);
-        assert_eq!(&data, packet.as_slice());
+        let packet = RequestPacket::<256>::from_slice(&REQUEST_DATA);
+        assert_eq!(REQUEST_DATA, packet.as_slice());
     }
-
-    // #[test]
-    // #[should_panic]
-    // fn test_request_packet_from_slice_panic() {
-    //     let data = [0, 1, 2];
-    //     RequestPacket::<256>::from_slice(&data);
-    // }
 
     #[test]
     fn test_request_packet_access_methods() {
-        let data = [0xE0, 0xFF, 3, 1, 2, 5];
-        let packet = dbg!(RequestPacket::<256>::from_slice(&data));
+        let packet = RequestPacket::<256>::from_slice(&REQUEST_DATA);
 
-        assert_eq!(packet.sync(), 0xE0);
-        assert_eq!(packet.dest(), 0xFF);
-        assert_eq!(packet.size(), 0x03);
-        assert_eq!(packet.data(), &[0x01, 0x02]);
-        assert_eq!(packet.checksum(), 0x05);
+        assert_eq!(packet.sync(), REQUEST_DATA[0]);
+        assert_eq!(packet.dest(), REQUEST_DATA[1]);
+        assert_eq!(packet.size(), REQUEST_DATA[2]);
+        assert_eq!(packet.data(), &[REQUEST_DATA[3], REQUEST_DATA[4]]);
+        assert_eq!(packet.checksum(), REQUEST_DATA[5]);
     }
 
     #[test]
@@ -88,23 +77,102 @@ mod tests {
         let mut packet = RequestPacket::<256>::new();
         packet
             .set_sync()
-            .set_dest(0xFF)
-            .set_size(0x03)
-            .set_data(&[0x01, 0x02])
-            .set_checksum(0x05);
-        assert_eq!(packet.as_slice(), [0xE0, 0xFF, 0x03, 0x01, 0x02, 0x05]);
+            .set_dest(REQUEST_DATA[1])
+            .set_data(&[REQUEST_DATA[3], REQUEST_DATA[4]])
+            .set_checksum(REQUEST_DATA[5])
+            .set_size(REQUEST_DATA[2]);
+
+        assert_eq!(packet.as_slice(), REQUEST_DATA);
         packet.calculate_checksum();
-        dbg!(&packet.as_slice());
-        assert_eq!(packet.checksum(), 0x05)
+        assert_eq!(packet.checksum(), REQUEST_DATA[5]);
+        packet.set_data(&[0x01]);
+        assert_eq!(packet.size(), REQUEST_DATA[2] - 1);
     }
 
     #[test]
     fn test_request_packet_read() {
         use crate::ReadPacket;
-        let mut data = std::io::Cursor::new([0xE0u8, 0, 3, 1, 2, 5]);
+        let mut cursor = std::io::Cursor::new(REQUEST_DATA);
         let mut packet = RequestPacket::<256>::new();
-        data.read_packet(&mut packet).unwrap();
+        cursor.read_packet(&mut packet).unwrap();
 
-        assert_eq!(&data.into_inner(), packet.as_slice())
+        assert_eq!(cursor.into_inner(), packet.as_slice())
+    }
+
+    #[test]
+    fn test_request_packet_write() {
+        use crate::WritePacket;
+        let mut writer = std::io::Cursor::new(vec![]);
+        let packet = RequestPacket::<256>::from_slice(&REQUEST_DATA);
+        writer.write_packet_with_checksum(&packet).unwrap();
+
+        assert_eq!(writer.into_inner(), packet.as_slice())
+    }
+
+
+    // Response Packet tests
+    #[test]
+    fn test_response_packet_from_slice() {
+        let packet = ResponsePacket::<256>::from_slice(&REQUEST_DATA);
+        assert_eq!(REQUEST_DATA, packet.as_slice());
+    }
+
+    // #[test]
+    // #[should_panic]
+    // fn test_response_packet_from_slice_panic() {
+    //     let data = [0, 1, 2];
+    //     ResponsePacket::<256>::from_slice(&data);
+    // }
+
+    #[test]
+    fn test_response_packet_access_methods() {
+        let packet = dbg!(ResponsePacket::<256>::from_slice(&RESPONSE_DATA));
+
+        assert_eq!(packet.sync(), RESPONSE_DATA[0]);
+        assert_eq!(packet.dest(), RESPONSE_DATA[1]);
+        assert_eq!(packet.size(), RESPONSE_DATA[2]);
+        assert_eq!(packet.report_raw(), RESPONSE_DATA[3]);
+        assert_eq!(packet.data(), &[RESPONSE_DATA[4], RESPONSE_DATA[5]]);
+        assert_eq!(packet.checksum(), RESPONSE_DATA[6]);
+    }
+
+    #[test]
+    fn test_response_packet_setter_methods() {
+        let mut packet = ResponsePacket::<256>::new();
+        packet
+            .set_sync()
+            .set_dest(RESPONSE_DATA[1])
+            .set_report(RESPONSE_DATA[3])
+            .set_data(&[RESPONSE_DATA[4], RESPONSE_DATA[5]])
+            .set_checksum(RESPONSE_DATA[6])
+            .set_size(RESPONSE_DATA[2]);
+
+        assert_eq!(packet.as_slice(), RESPONSE_DATA);
+        packet.calculate_checksum();
+        assert_eq!(packet.checksum(), RESPONSE_DATA[6]);
+        packet.set_data(&[0x01]);
+        assert_eq!(packet.size(), RESPONSE_DATA[2] - 1);
+    }
+
+    #[test]
+    fn test_response_packet_read() {
+        use crate::ReadPacket;
+        let mut reader = std::io::Cursor::new(RESPONSE_DATA);
+        let mut packet = ResponsePacket::<256>::new();
+        reader.read_packet(&mut packet).unwrap();
+
+        assert_eq!(reader.into_inner(), packet.as_slice())
+    }
+
+    #[test]
+    fn test_response_packet_write() {
+        use crate::WritePacket;
+        let mut writer = std::io::Cursor::new(vec![]);
+        let packet = ResponsePacket::<256>::from_slice(&RESPONSE_DATA);
+        writer.write_packet_with_checksum(&packet).unwrap();
+
+        assert_eq!(writer.into_inner(), packet.as_slice())
     }
 }
+
+
--- src/jvs_modified.rs
+++ src/jvs_modified.rs
@@ -93,43 +93,31 @@ impl<const N: usize> ResponsePacket<N> {
 
 impl_required_packet_blocks!(ResponsePacket);
 
-// TODO: Complete writing tests
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    // #[test]
-    // #[should_panic]
-    // fn test_request_packet_new_panic() {
-    //     let _ = RequestPacket::<1>::new();
-    // }
+    const REQUEST_DATA: [u8; 8] = [0xE0, 0x06, 0xFF, 0x01, 0x02, 0x01, 0x02, 0x0B];
+    const RESPONSE_DATA: [u8; 10] = [0xE0, 0x08, 0xFF, 0x01, 0x03, 0x02, 0x04, 0x01, 0x02, 0x14];
 
+    // Request Packet tests
     #[test]
     fn test_request_packet_from_slice() {
-        let data = [0xE0, 6, 3, 1, 2, 1, 2, 14];
-        let packet = RequestPacket::<256>::from_slice(&data);
-        assert_eq!(&data, packet.as_slice());
+        let packet = RequestPacket::<256>::from_slice(&REQUEST_DATA);
+        assert_eq!(REQUEST_DATA, packet.as_slice());
     }
-
-    // #[test]
-    // #[should_panic]
-    // fn test_request_packet_from_slice_panic() {
-    //     let data = [0, 1, 2];
-    //     RequestPacket::<256>::from_slice(&data);
-    // }
 
     #[test]
     fn test_request_packet_access_methods() {
-        let data = [0xE0, 6, 3, 1, 2, 1, 2, 14];
-        let packet = dbg!(RequestPacket::<256>::from_slice(&data));
+        let packet = dbg!(RequestPacket::<256>::from_slice(&REQUEST_DATA));
 
-        assert_eq!(packet.sync(), data[0]);
-        assert_eq!(packet.size(), data[1]);
-        assert_eq!(packet.dest(), data[2]);
-        assert_eq!(packet.sequence(), data[3]);
-        assert_eq!(packet.cmd(), data[4]);
-        assert_eq!(packet.data(), &[data[5], data[6]]);
-        assert_eq!(packet.checksum(), data[7]);
+        assert_eq!(packet.sync(), REQUEST_DATA[0]);
+        assert_eq!(packet.size(), REQUEST_DATA[1]);
+        assert_eq!(packet.dest(), REQUEST_DATA[2]);
+        assert_eq!(packet.sequence(), REQUEST_DATA[3]);
+        assert_eq!(packet.cmd(), REQUEST_DATA[4]);
+        assert_eq!(packet.data(), &[REQUEST_DATA[5], REQUEST_DATA[6]]);
+        assert_eq!(packet.checksum(), REQUEST_DATA[7]);
     }
 
     #[test]
@@ -137,26 +125,109 @@ mod tests {
         let mut packet = RequestPacket::<256>::new();
         packet
             .set_sync()
-            .set_dest(0xFF)
-            .set_sequence(0x01)
-            .set_cmd(0x02)
-            .set_data(&[0x01, 0x02])
-            
-            .set_checksum(0x05);
-        assert_eq!(packet.as_slice(), [0xE0, 0xFF, 0x03, 0x01, 0x02, 0x05]);
+            .set_dest(REQUEST_DATA[2])
+            .set_sequence(REQUEST_DATA[3])
+            .set_cmd(REQUEST_DATA[4])
+            .set_data(&[REQUEST_DATA[5], REQUEST_DATA[6]])
+            .set_checksum(REQUEST_DATA[7])
+            .set_size(REQUEST_DATA[1]);
+
+        assert_eq!(packet.as_slice(), REQUEST_DATA);
         packet.calculate_checksum();
-        dbg!(&packet.as_slice());
-        assert_eq!(packet.checksum(), 0x05)
+        assert_eq!(packet.checksum(), REQUEST_DATA[7]);
+        packet.set_data(&[0x01]);
+        assert_eq!(packet.size(), REQUEST_DATA[1] - 1);
     }
 
     #[test]
     fn test_request_packet_read() {
         use crate::ReadPacket;
-        let mut data = std::io::Cursor::new([0xE0u8, 0, 3, 1, 2, 5]);
+        let mut cursor = std::io::Cursor::new(REQUEST_DATA);
         let mut packet = RequestPacket::<256>::new();
-        data.read_packet(&mut packet).unwrap();
+        cursor.read_packet(&mut packet).unwrap();
 
-        assert_eq!(&data.into_inner(), packet.as_slice())
+        assert_eq!(cursor.into_inner(), packet.as_slice())
+    }
+
+    #[test]
+    fn test_request_packet_write() {
+        use crate::WritePacket;
+        let mut writer = std::io::Cursor::new(vec![]);
+        let packet = RequestPacket::<256>::from_slice(&REQUEST_DATA);
+        writer.write_packet_with_checksum(&packet).unwrap();
+
+        assert_eq!(writer.into_inner(), packet.as_slice())
+    }
+
+
+    // Response Packet tests
+    #[test]
+    fn test_response_packet_from_slice() {
+        let packet = ResponsePacket::<256>::from_slice(&REQUEST_DATA);
+        assert_eq!(REQUEST_DATA, packet.as_slice());
+    }
+
+    // #[test]
+    // #[should_panic]
+    // fn test_response_packet_from_slice_panic() {
+    //     let data = [0, 1, 2];
+    //     ResponsePacket::<256>::from_slice(&data);
+    // }
+
+    #[test]
+    fn test_response_packet_access_methods() {
+        let packet = ResponsePacket::<256>::from_slice(&RESPONSE_DATA);
+
+        assert_eq!(packet.sync(), RESPONSE_DATA[0]);
+        assert_eq!(packet.size(), RESPONSE_DATA[1]);
+        assert_eq!(packet.dest(), RESPONSE_DATA[2]);
+        assert_eq!(packet.sequence(), RESPONSE_DATA[3]);
+        assert_eq!(packet.status(), RESPONSE_DATA[4]);
+        assert_eq!(packet.cmd(), RESPONSE_DATA[5]);
+        assert_eq!(packet.report_raw(), RESPONSE_DATA[6]);
+        assert_eq!(packet.data(), &[RESPONSE_DATA[7], RESPONSE_DATA[8]]);
+        assert_eq!(packet.checksum(), RESPONSE_DATA[9]);
+    }
+
+    #[test]
+    fn test_response_packet_setter_methods() {
+        let mut packet = ResponsePacket::<256>::new();
+        packet
+            .set_sync()
+            .set_dest(RESPONSE_DATA[2])
+            .set_sequence(RESPONSE_DATA[3])
+            .set_status(RESPONSE_DATA[4])
+            .set_cmd(RESPONSE_DATA[5])
+            .set_report(RESPONSE_DATA[6])
+            .set_data(&[RESPONSE_DATA[7], RESPONSE_DATA[8]])
+            .set_checksum(RESPONSE_DATA[9])
+            .set_size(RESPONSE_DATA[1]);
+
+        assert_eq!(packet.as_slice(), RESPONSE_DATA);
+        packet.calculate_checksum();
+        assert_eq!(packet.checksum(), RESPONSE_DATA[9]);
+        packet.set_data(&[0x01]);
+        assert_eq!(packet.size(), RESPONSE_DATA[1] - 1);
+    }
+
+    #[test]
+    fn test_response_packet_read() {
+        use crate::ReadPacket;
+        let mut reader = std::io::Cursor::new(RESPONSE_DATA);
+        let mut packet = ResponsePacket::<256>::new();
+        reader.read_packet(&mut packet).unwrap();
+
+        assert_eq!(reader.into_inner(), packet.as_slice())
+    }
+
+    #[test]
+    fn test_response_packet_write() {
+        use crate::WritePacket;
+        let mut writer = std::io::Cursor::new(vec![]);
+        let packet = ResponsePacket::<256>::from_slice(&RESPONSE_DATA);
+        writer.write_packet_with_checksum(&packet).unwrap();
+
+        assert_eq!(writer.into_inner(), packet.as_slice())
     }
 }
 
